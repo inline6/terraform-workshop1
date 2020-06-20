bc467c26dab5c5176694e59706bd6db64057c171
inline6 <bena.mihai@gmail.com>
2020-06-20 13:52:41 +0300

    added backend

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -1,4 +1,14 @@
 # Configure the AWS Provider
 provider "aws" {
-  region  = "eu-central-1"
+  region = "eu-central-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket1990"
+    key            = "development/terraform.tfstate"
+    region         = "eu-central-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
 }
