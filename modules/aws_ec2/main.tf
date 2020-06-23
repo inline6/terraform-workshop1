a82b6e835f797bf72ce7ece1193e66fbc85207cd
inline6 <bena.mihai@gmail.com>
2020-06-23 22:04:11 +0300

    Adding the resource

--- modules/aws_ec2/main.tf
+++ modules/aws_ec2/main.tf
@@ -1,34 +1,36 @@
-# EC2 instance
-resource "aws_instance" "web" {
-  ami           = "ami-0e342d72b12109f91"
-  instance_type = "t2.micro"
-  security_groups = aws_security_group.allow_tls
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "Terraform-workshop"
-    }
+    Name = "tf-example"
+  }
 }
 
-# Security group for AWS instance
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-
-  ingress {
-    description = "TLS from VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = "${aws_vpc.my_vpc.id}"
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-central-1a"
 
   tags = {
-    Name = "allow_tls"
+    Name = "tf-example"
+  }
+}
+
+resource "aws_network_interface" "networkint" {
+  subnet_id   = "${aws_subnet.my_subnet.id}"
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "terraform-workshop1" {
+  ami           = "ami-0e342d72b12109f91"
+  instance_type = "t2.micro"
+
+  network_interface {
+    network_interface_id = "${aws_network_interface.networkint.id}"
+    device_index         = 0
   }
 }
